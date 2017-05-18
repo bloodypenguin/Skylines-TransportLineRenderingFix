d016e545f646d81e34fbc23e69c1267a5fcd79b0
Egor Aralov <aei.omsk@gmail.com>
2017-05-18 19:03:36 +0200

    Fix for 1.7

--- TransportLineRenderingFix/Detours/TransportLineDetour.cs
+++ TransportLineRenderingFix/Detours/TransportLineDetour.cs
@@ -12,21 +12,34 @@ namespace TransportLineRenderingFix.Detours
         {
             if (line.m_flags == TransportLine.Flags.None)
                 return;
-            TransportInfo info =line.Info;
+            TransportInfo info = line.Info;
             //begin mod
             if ((1 << (int)(info.m_transportType & (TransportInfo.TransportType)31) & typeMask) == 0)
                 return;
             //end mod
-            TransportManager instance = Singleton<TransportManager>.instance;
-            Mesh mesh = instance.m_lineMeshes[(int) lineID];
-            if (!((UnityEngine.Object) mesh != (UnityEngine.Object) null))
+            TransportManager instance1 = Singleton<TransportManager>.instance;
+            TerrainManager instance2 = Singleton<TerrainManager>.instance;
+            Mesh[] lineMesh = instance1.m_lineMeshes[(int)lineID];
+            if (lineMesh == null)
                 return;
-            Material material = info.m_lineMaterial2;
-            material.color = line.GetColor();
-            if (!material.SetPass(0))
-                return;
-            ++instance.m_drawCallData.m_overlayCalls;
-            Graphics.DrawMeshNow(mesh, Matrix4x4.identity);
+            int length = lineMesh.Length;
+            for (int index = 0; index < length; ++index)
+            {
+                Mesh mesh = lineMesh[index];
+                if ((UnityEngine.Object)mesh != (UnityEngine.Object)null && cameraInfo.Intersect(mesh.bounds))
+                {
+                    Material lineMaterial2 = info.m_lineMaterial2;
+                    lineMaterial2.color = line.GetColor();
+                    lineMaterial2.SetFloat(instance1.ID_StartOffset, -1000f);
+                    if (info.m_requireSurfaceLine)
+                        instance2.SetWaterMaterialProperties(mesh.bounds.center, lineMaterial2);
+                    if (lineMaterial2.SetPass(0))
+                    {
+                        ++instance1.m_drawCallData.m_overlayCalls;
+                        Graphics.DrawMeshNow(mesh, Matrix4x4.identity);
+                    }
+                }
+            }
         }
     }
 }
