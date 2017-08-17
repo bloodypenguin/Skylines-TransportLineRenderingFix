d4b1744e034c24096df02e286296bd1f11f76cb2
Egor Aralov <aei.omsk@gmail.com>
2017-08-17 20:44:49 +0200

    Update for 1.8.0

--- TransportLineRenderingFix/LoadingExtension.cs
+++ TransportLineRenderingFix/LoadingExtension.cs
@@ -1,6 +1,4 @@
 ﻿using ICities;
-using TransportLineRenderingFix.Detours;
-using TransportLineRenderingFix.Redirection;
 
 namespace TransportLineRenderingFix
 {
@@ -8,37 +6,15 @@ namespace TransportLineRenderingFix
     {
         public override void OnLevelLoaded(LoadMode mode)
         {
-            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame && mode != LoadMode.NewGameFromScenario)
+            base.OnLevelLoaded(mode);
+            var metro = PrefabCollection<TransportInfo>.FindLoaded("Metro");
+            var train = PrefabCollection<TransportInfo>.FindLoaded("Train");
+            if (train == null || metro == null)
             {
                 return;
             }
-            base.OnLevelLoaded(mode);
-            Redirector<TransportLineDetour>.Deploy();
-            var metro = PrefabCollection<TransportInfo>.FindLoaded("Metro");
-            var train = PrefabCollection<TransportInfo>.FindLoaded("Train");
-            if (train != null)
-            {
-                train.m_lineMaterial.shader = metro.m_lineMaterial.shader;
-                train.m_lineMaterial2.shader = metro.m_lineMaterial2.shader;
-            }
-            var bus = PrefabCollection<TransportInfo>.FindLoaded("Bus");
-            if (bus != null)
-            {
-                bus.m_lineMaterial.shader = metro.m_lineMaterial.shader;
-                bus.m_lineMaterial2.shader = metro.m_lineMaterial2.shader;
-            }
-            var tram = PrefabCollection<TransportInfo>.FindLoaded("Tram");
-            if (tram != null)
-            {
-                tram.m_lineMaterial.shader = metro.m_lineMaterial.shader;
-                tram.m_lineMaterial2.shader = metro.m_lineMaterial2.shader;
-            }
-        }
-
-        public override void OnLevelUnloading()
-        {
-            base.OnLevelUnloading();
-            Redirector<TransportLineDetour>.Revert();
+            metro.m_lineMaterial.shader = train.m_lineMaterial.shader;
+            metro.m_lineMaterial2.shader = train.m_lineMaterial2.shader;
         }
     }
 }
